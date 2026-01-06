864b6e6f94d638b8fdccaa67c2d72da1319bc758
Uranite <62639703+Uranite@users.noreply.github.com>
2026-01-06 11:10:07 +0700

    Update svtav1.cmake

--- packages/svtav1.cmake
+++ packages/svtav1.cmake
@@ -1,3 +1,5 @@
+message(STATUS "SVT-AV1 FLAGS: CXX='${CMAKE_CXX_FLAGS} -flto -O2 -DNDEBUG' C='${CMAKE_C_FLAGS} -flto -O2 -DNDEBUG'")
+
 ExternalProject_Add(svtav1
     GIT_REPOSITORY https://gitlab.com/AOMediaCodec/SVT-AV1.git
     SOURCE_DIR ${SOURCE_LOCATION}
@@ -12,19 +14,10 @@ ExternalProject_Add(svtav1
         -DBUILD_SHARED_LIBS=OFF
         -DENABLE_AVX512=ON
         -DBUILD_TESTING=OFF
-        -DBUILD_ENC=ON
         -DSVT_AV1_LTO=OFF
         -DBUILD_APPS=OFF
-        -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -Dav1_cospi_arr_s32_data=svtav1_av1_cospi_arr_s32_data
-                         -Dav1_fwd_txfm2d_16x16_avx512=svtav1_av1_fwd_txfm2d_16x16_avx512
-                         -Dav1_fwd_txfm2d_32x32_avx512=svtav1_av1_fwd_txfm2d_32x32_avx512
-                         -Dav1_fwd_txfm2d_64x64_avx512=svtav1_av1_fwd_txfm2d_64x64_avx512
-                         -Dav1_fwd_txfm2d_32x64_avx512=svtav1_av1_fwd_txfm2d_32x64_avx512
-                         -Dav1_fwd_txfm2d_64x32_avx512=svtav1_av1_fwd_txfm2d_64x32_avx512
-                         -Dav1_fwd_txfm2d_16x64_avx512=svtav1_av1_fwd_txfm2d_16x64_avx512
-                         -Dav1_fwd_txfm2d_64x16_avx512=svtav1_av1_fwd_txfm2d_64x16_avx512
-                         -Dav1_fwd_txfm2d_16x32_avx512=svtav1_av1_fwd_txfm2d_16x32_avx512
-                         -Dav1_fwd_txfm2d_32x16_avx512=svtav1_av1_fwd_txfm2d_32x16_avx512'
+        -DCMAKE_CXX_FLAGS_RELEASE='${CMAKE_CXX_FLAGS} -flto -O2 -DNDEBUG'
+        -DCMAKE_C_FLAGS_RELEASE='${CMAKE_C_FLAGS} -flto -O2 -DNDEBUG'
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
